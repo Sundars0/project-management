d2c71f0cab8c96e107bcfdd4c539a560fdc62f5e
Sundars <Sundars04@rediffmail.com>
2016-10-01 13:45:24 +0000

    Add styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,24 @@
 class TasksController < ApplicationController
   before_action :set_project
+  before_action :set_task, except: [:create] 
   def create    
     @task = @project.tasks.create(task_params)
     redirect_to @project
   end
 
   def destroy
-    @task = @project.tasks.find(params[:id])
+    
     if @task.destroy
       flash[:success] = "Task was deleted"
     else
       flash[:error] = "Task was not deleted"
     end
     redirect_to @project
+  end
+
+  def complete
+    @task.update_attribute(:completed_at, Time.now)
+    redirect_to @project, notice: "Task Completed"
   end
 
   private
@@ -23,6 +29,10 @@ class TasksController < ApplicationController
 
   def task_params
     params[:task].permit(:content)
+  end
+
+  def set_task
+    @task = @project.tasks.find(params[:id])    
   end
 
 
